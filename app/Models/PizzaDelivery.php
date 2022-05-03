47162f6cd369909e37ce37e74dcdbe537b393171
Alex Groat <alex.groat.95@gmail.com>
2022-05-03 10:41:08 +0930

    open closed principle pizza delivery concept init commit

---
+++ app/Models/PizzaDelivery.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car {}
+
+class Boat {}
+
+class PizzaDelivery extends Model
+{
+    use HasFactory;
+
+    public function deliver()
+    {
+        $prepare = 'place pizza in car';
+        $travel = ' and drive down the road';
+        $customer = ' then give pizza to happy customer';
+        return $prepare.$travel.$customer;
+    }
+}
+
+$pizza = new PizzaDelivery();
+echo $pizza->deliver();
+
+/* here is a perfectly good class that is able to deliver pizza in a fine manner. What if pizzas needed
+to be delivered by boat.*/
